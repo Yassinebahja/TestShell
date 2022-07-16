e72be55b830805f114c2ee55f493f59654b34fc6
wind <fatima.ezzahra.gabri@gmail.com>
2022-07-16 02:40:09 +0200

    update strncmp

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <string.h>
 //we do until n-2, because the last assignment is n-1, since we increment i
 //https://www.geeksforgeeks.org/stdstrncmp-in-c/
 
-int ft_strcmp(char *s1, char *s2, unsigned int n){
-    int i = 0;
+int ft_strncmp(char *s1, char *s2, unsigned int n){
+    unsigned int i = 0;
     
-    while(((s1[i] -  s2[i]) == 0) && s1[i] && i < n-1) 
-       ++i;
-    
-    return s1[i] - s2[i];
+    while(((s1[i] != '\0') || s2[i] != '\0') && i<n)  {
+       if(s1[i] > s2[i]) {
+           return 1;
+       }
+       else if (s1[i] < s2[i]){
+           return -1;
+       }
+       i++;
+    }
+    return 0;
 
 }
 
-
-int main(){
-    char s1[20] ="abc";
-    char s2[20] = "abcrjkdl";
-    printf("%d",ft_strcmp(s1,s2,3));
-
+int	main()
+{
+	printf("%d", ft_strncmp("123", "1234", 5));
+    printf("\n%d",strncmp("123", "1234", 5));
 }
-
