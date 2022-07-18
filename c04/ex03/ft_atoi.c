56ca3f5870b63c0a03f0292db0f9ea66991f8431
wind <fatima.ezzahra.gabri@gmail.com>
2022-07-18 16:50:56 +0200

    update atoi function

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int ft_atoi( char *str){
  
@@ -7,12 +8,13 @@ int ft_atoi( char *str){
 	int count = 0;
 	int i=0;
 	int number = 0;
-	while((str[i] >= 9 && str[i] <= 13) || str[i] == ' ' || str[i] == '-' || str[i] == '+') {
-	 	if ( str[i] == '-') 
-		      	count++;
+	while((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		i++;
-		
-	
+
+	while(str[i] == '+' || str[i] == '-') {
+		if(str[i] == '-')
+			count++;
+		i++;
 	}
 
 	while(str[i] >= 48 && str[i] <= 57){
@@ -34,6 +36,7 @@ int ft_atoi( char *str){
 
 int main(){
 
-	char str[] = "		abc56782ab123";
+	char str[] = "		-+-378?iuh987ghj";
 	printf("%d",ft_atoi(str));
+	printf("\n%d",atoi(str));
 }
