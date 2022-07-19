5a6afcc37c0d41ddc0d9456adada20e4f0b0d6f2
wind <fatima.ezzahra.gabri@gmail.com>
2022-07-19 21:47:27 +0200

    added

---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int ft_recursive_power(int nb, int power){
+
+	if (power <1)
+		return !power;
+		
+        return ( nb * ft_recursive_power(nb, power - 1));
+
+}
+//in recursive function we don't use condition for return the recursive funtion in question, and we need to write all conditions above the the last return and don't write else.
+int main (){
+
+	printf("%d", ft_recursive_power(3,3));
+}
+
