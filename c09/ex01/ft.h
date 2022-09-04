5ce765b7fe177cb8157a9b2854bee1f1e6dc5833
pinwheelLorem <fatima.ezzahra.gabri@gmail.com>
2022-09-04 23:34:28 +0200

    makefile

---
+++ c09/ex01/ft.h
@@ -0,0 +1,34 @@
+#ifndef FT_H
+#define FT_H
+
+void ft_swap(int *a, int *b);
+
+#endif
+
+
+
+// #include "../ex01/ft.h"
+// #include <stdio.h>
+// void ft_swap(int *a, int *b){
+
+//     int c;
+//     c = *a;
+//     *a = *b;
+//     *b = c;
+
+// }
+
+
+// main.c:
+
+// #include "../ex01/ft.h"
+// #include <stdio.h>
+
+// int main(){
+
+//     int a=12;
+//     int b=23;
+//     ft_swap(&a,&b);
+//     printf("a: %d & b: %d", a ,b);
+
+// }
